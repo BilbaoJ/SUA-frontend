7dc97d8a06cb61cb3a9b3fd0ad45d6514f0679ae
BilbaoJ <jessicabilbao02@gmail.com>
2024-12-03 14:16:07 -0500

    Responsive footer

--- src/components/shared/FooterComponent.vue
+++ src/components/shared/FooterComponent.vue
@@ -1,10 +1,12 @@
 <template>
-  <footer class="grid grid-cols-1 w-full space-y-6 mb-20 py-6 mt-6 text-center text-accent">
-    <div class="w-full">
+  <footer
+    class="grid grid-cols-1 md:grid-cols-2 place-items-center w-full space-y-6 mb-20 py-6 mt-6 text-center text-accent max-w-4xl mx-auto"
+  >
+    <div class="w-full place-self-center">
       <h2 class="text-xl font-bold">Developed by Jessica Bilbao</h2>
       <p class="text-sm">Fullstack Developer</p>
     </div>
-    <div class="w-full text-sm">
+    <div class="w-full text-sm place-self-center">
       <p>
         Email:
         <a href="mailto:jessicabilbao02@gmail.com" class="text-blue-400"
@@ -17,7 +19,7 @@
       </p>
     </div>
 
-    <div class="w-full text-sm">
+    <div class="w-full text-sm md:col-span-2 place-self-center">
       <p>© 2024 BilbaoJ. All rights reserved.</p>
     </div>
   </footer>
