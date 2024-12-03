e8b0ff17e8938af234b51c8c9e2e29b9ee061dea
BilbaoJ <jessicabilbao02@gmail.com>
2024-12-03 14:06:37 -0500

    Add footer

---
+++ src/components/shared/FooterComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="grid grid-cols-1 w-full space-y-6 mb-20 py-6 mt-6 text-center text-accent">
+    <div class="w-full">
+      <h2 class="text-xl font-bold">Developed by Jessica Bilbao</h2>
+      <p class="text-sm">Fullstack Developer</p>
+    </div>
+    <div class="w-full text-sm">
+      <p>
+        Email:
+        <a href="mailto:jessicabilbao02@gmail.com" class="text-blue-400"
+          >jessicabilbao02@gmail.com</a
+        >
+      </p>
+      <p>
+        Link to my GitHub:
+        <a href="https://github.com/BilbaoJ" target="_blank" class="text-blue-400">GitHub</a>
+      </p>
+    </div>
+
+    <div class="w-full text-sm">
+      <p>© 2024 BilbaoJ. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HeaderComponent from '@/components/shared/HeaderComponent.vue'
+import FooterComponent from '@/components/shared/FooterComponent.vue'
 import List from '@/components/posts/PostsList.vue'
 </script>
 <template>
@@ -7,4 +8,5 @@ import List from '@/components/posts/PostsList.vue'
   <div class="flex flex-col bg-light">
     <List />
   </div>
+  <FooterComponent />
 </template>
